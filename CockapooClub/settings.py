2670c5461e6b4647117133aab1cb2c40f79fcd8d
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-11-18 11:55:22 +0000

    fixed commented out root_dir in settings.py file

--- CockapooClub/settings.py
+++ CockapooClub/settings.py
@@ -138,7 +138,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'Cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
