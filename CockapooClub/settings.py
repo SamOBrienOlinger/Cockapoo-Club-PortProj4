90733e146fa0cb87c3d7d12cc39dde5441135256
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-11-19 16:19:26 +0000

    update settings file

--- CockapooClub/settings.py
+++ CockapooClub/settings.py
@@ -99,6 +99,9 @@ DATABASES = {
    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+# DATABASES = {
+#    'default': dj_database_url.parse('postgres: //https://book-sessions.herokuapp.com'
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
