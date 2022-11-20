7a202dd339b60c996d9a38dbf56a3bac89b41577
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-11-20 18:21:15 +0000

    starting with class booking(models.Model)

--- book_sessions/models.py
+++ book_sessions/models.py
@@ -1,6 +1,11 @@
 from django.db import models
-
-# Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+
+# Create your models here.
+
+
+class booking(models.Model):
+
+    def
