15c24655e722535afef3446455bcbaf9a93a342f
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-11-20 15:00:59 +0000

    migrations made after changing database url in env

--- CockapooClub/settings.py
+++ CockapooClub/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['home-cockapoo-club-pp4.herokuapp.com', 'book-sessions.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['home-cockapoo-club-pp4.herokuapp.com', 'book-sessions.herokuapp.com', 'localhost']
 
 
 # Application definition
