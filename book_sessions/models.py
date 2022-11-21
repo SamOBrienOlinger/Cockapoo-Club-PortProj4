c61d9cd6338eb5bacba381d6b4e92b3d96f7e5dc
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-11-21 14:24:06 +0000

    account and openid folders added to templates

--- book_sessions/models.py
+++ book_sessions/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 
-class booking(models.Model):
+# class booking(models.Model):
 
-    def
+#     def
