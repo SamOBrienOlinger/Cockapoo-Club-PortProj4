3e90d65fa461efc85e364d5fcbdf652ce0e3316c
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-11-22 12:08:55 +0000

    install summernote

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from myproject.myapp.models import Author
+from CockapooClub.home.models import Author
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 
@@ -7,3 +9,23 @@ from myproject.myapp.models import Author
 class AuthorAdmin(admin.ModelAdmin):
     pass
     admin.site.register(Author, AuthorAdmin)
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
