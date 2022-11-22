c8e51fd73a6a386ab91a0a6c0d57b12a6ef0dc74
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-11-22 11:42:03 +0000

    add description of functions/classes - both models

--- book_sessions/models.py
+++ book_sessions/models.py
@@ -1,33 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-# from six import python_2_unicode_compatible
 
 # Create your models here.
 
-STATUS = ((0, "Draft"), (1, "Published"))
 
+# class book a time slot for a training session:
 
-class book(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
-    featured_image = CloudinaryField('image', default='placeholder')
-    # excerpt = models.TextField(blank=True)
-    # updated_on = models.DateTimeField(auto_now=True)
-    # content = models.TextField()
-    # created_on = models.DateTimeField(auto_now_add=True)
-    # status = models.IntegerField(choices=STATUS, default=0)
-    # likes = models.ManyToManyField(
-    #     User, related_name='blogpost_like', blank=True)
+    # allow user to: 
+      # Choose time
+      # Choose date
 
-    # class Meta:
-    #     ordering = ["-created_on"]
+      # Submit button
+      # Edit button
+      # delete button
 
-    # def __str__(self):
-    #     return self.title
-
-    # def number_of_likes(self):
-    #     return self.likes.count()
+# class book(models.Model):
+    # title = models.CharField(max_length=200, unique=True)
+    # slug = models.SlugField(max_length=200, unique=True)
+    # featured_image = CloudinaryField('image', default='placeholder')
--- home/models.py
+++ home/models.py
@@ -3,3 +3,23 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+# class signup:
+
+    # create username and password 
+
+    # enter/register details: 
+        # Username
+        # Password
+        # User/Owner name
+        # Dog name
+        # Dog age
+        # Vaccinated (Y/N)
+        # user submit button
+ 
+
+
+
+# class signin
+    # authenticate username and password to login
+    # bring user to booking page
