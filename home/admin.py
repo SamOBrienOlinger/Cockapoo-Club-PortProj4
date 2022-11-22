7e6b693cc324a01468acaa7185b272b207a8953d
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-11-22 12:03:23 +0000

    class AuthorAdmin added to admin.py in home app

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from myproject.myapp.models import Author
 
 # Register your models here.
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+    admin.site.register(Author, AuthorAdmin)
