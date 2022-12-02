5d1c17edbd4562853e7d5eecc2171f95eb5802a1
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-12-02 12:51:36 +0000

    remove commented out text from home urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,3 @@
-# from . import views
-# from django.urls import path
-# from home import views
-
-# urlpatterns = [
-#     path('', views.homepage, name="homepage"),
-# ]
-
 from django.urls import path
 from . import views
 
