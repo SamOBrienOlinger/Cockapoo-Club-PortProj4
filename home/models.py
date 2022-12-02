859ed1a7210d72533ff2eff57e7b288fd5cb1466
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-12-02 14:19:18 +0000

    update home app with index

--- home/models.py
+++ home/models.py
@@ -5,7 +5,7 @@ from cloudinary.models import CloudinaryField
 # Create your models here.
 
 
-class homepage(models.Model):
+class index(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
 
