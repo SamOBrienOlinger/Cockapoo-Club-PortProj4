422a69b4888650836aac7fd1547f01cb676a970e
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-12-04 12:27:04 +0000

    content added to fffg, kyph, nc html templates

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.furry-fun-foto-gallery, name='furry-fun-foto-gallery'),
+    path('', views.Furry-Fun-Foto-Gallery, name='Furry-Fun-Foto-Gallery'),
     path('', views.New-Cockaparents, name='New-Cockaparents'),
     path('', views.Keep-your-Poo-Healthy, name='Keep-your-Poo-Healthy'),
     # path('', views.join-form, name='join-form.'),
