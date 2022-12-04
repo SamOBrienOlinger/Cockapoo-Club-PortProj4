b58b8500faf4723eeea6b91adc9871cc4b915f6b
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-12-04 12:38:56 +0000

    functions and urls bug fixed

--- home/urls.py
+++ home/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.Furry-Fun-Foto-Gallery, name='Furry-Fun-Foto-Gallery'),
-    path('', views.New-Cockaparents, name='New-Cockaparents'),
-    path('', views.Keep-your-Poo-Healthy, name='Keep-your-Poo-Healthy'),
+    path('', views.furryFunFotoGallery, name='Furry-Fun-Foto-Gallery'),
+    path('', views.newCockaparents, name='New-Cockaparents'),
+    path('', views.keepYourPooHealthy, name='Keep-your-Poo-Healthy'),
     # path('', views.join-form, name='join-form.'),
     # path('', views.login-members, name='login-members'),
 ]
