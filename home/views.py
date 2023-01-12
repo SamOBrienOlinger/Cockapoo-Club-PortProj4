f472fd1ab769907b1bf530af0348b1ea44383c8d
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-01-12 11:57:42 +0000

    import authenticate to home views.py

--- home/views.py
+++ home/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from .models import index
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 # from .forms import DogProfile
 
 # Create your views here.
