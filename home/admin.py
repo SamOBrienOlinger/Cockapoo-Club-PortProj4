57238aa1f09d262abd52152e45a3575b3ea81778
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-01-25 11:31:22 +0000

    add content to README.md file

--- home/admin.py
+++ home/admin.py
@@ -21,23 +21,3 @@ admin.site.register(index)
 # class AuthorAdmin(admin.ModelAdmin):
 #     pass
 #     admin.site.register(Author, AuthorAdmin)
-
-# @admin.register(Post)
-# class PostAdmin(SummernoteModelAdmin):
-
-#     list_display = ('title', 'slug', 'status', 'created_on')
-#     search_fields = ['title', 'content']
-#     list_filter = ('status', 'created_on')
-#     prepopulated_fields = {'slug': ('title',)}
-#     summernote_fields = ('content',)
-
-
-# @admin.register(Comment)
-# class CommentAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'body', 'post', 'created_on', 'approved')
-#     list_filter = ('approved', 'created_on')
-#     search_fields = ('name', 'email', 'body')
-#     actions = ['approve_comments']
-
-#     def approve_comments(self, request, queryset):
-#         queryset.update(approved=True)
