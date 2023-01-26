a7aad2fe375e37898ff419755ec6c3447bd81f6b
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-01-26 18:38:22 +0000

    Add Python Testing results and tidy code

--- book_sessions/admin.py
+++ book_sessions/admin.py
@@ -4,56 +4,8 @@ from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 
-
-# admin.register(booking)
-
-
-# class BookingAdmin(admin.ModelAdmin):
-#     list_display = ("user_email", "user_name", "date", "time", "approved")
-#     list_filter = ("approved", "date")
-#     ordering = ("date", "time")
-#     search_fields = ("user_email", "user_name")
-
 admin.site.register(Booking)
-
 
 admin.site.register(Home)
 
 admin.site.register(Booking_detail)
-
-# admin.site.register(BookingForm)
-# @admin.register(booking)
-# class PostAdmin(SummernoteModelAdmin):
-#     list_display = ('title', 'slug', 'status', 'created_on')
-#     search_fields = ['title', 'content']
-#     list_filter = ('status', 'created_on')
-#     prepopulated_fields = {'slug': ('title',)}
-#     summernote_fields = ('content',)
-
-
-# @admin.register(BookingForm)
-# class CommentAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'body', 'post', 'created_on', 'approved')
-#     list_filter = ('approved', 'created_on')
-#     search_fields = ('name', 'email', 'body')
-#     actions = ['approve_comments']
-
-#     def approve_comments(self, request, queryset):
-#         queryset.update(approved=True)
-
-
-# @admin.site.register(booking)
-# class booking(models.Model):
-#     title = models.CharField(max_length=200, unique=True)
-#     slug = models.SlugField(max_length=200, unique=True)
-
-
-# @admin.site.register(home)
-# class home(models.Model):
-#     title = models.CharField(max_length=200, unique=True)
-#     slug = models.SlugField(max_length=200, unique=True)
-
-# @admin.site.register(PersonalDataForm)
-# class PersonalDataForm(models.Model):
-#     title = models.CharField(max_length=200, unique=True)
-#     slug = models.SlugField(max_length=200, unique=True)
