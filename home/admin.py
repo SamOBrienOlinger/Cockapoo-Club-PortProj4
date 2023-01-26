83bc56a0ba2dfc6d50b7357a8fdb7bc121bade71
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-01-26 18:38:46 +0000

    Add Python Testing results and tidy code

--- home/admin.py
+++ home/admin.py
@@ -4,20 +4,3 @@ from .models import index
 
 admin.site.register(index)
 
-# @admin.site.register(home())
-# class home(models.Model):
-#     title = models.CharField(max_length=200, unique=True)
-#     slug = models.SlugField(max_length=200, unique=True)
-
-# admin.site.register(homepage)
-
-# from CockapooClub.home.models import Author
-# from .models import Post, Comment
-# from django_summernote.admin import SummernoteModelAdmin
-
-# Register your models here.
-
-
-# class AuthorAdmin(admin.ModelAdmin):
-#     pass
-#     admin.site.register(Author, AuthorAdmin)
